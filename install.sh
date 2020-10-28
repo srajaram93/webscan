000d2509c187991ab3da9165866cf114a17528fe
srajaram93 <srajaram93@gmail.com>
2020-10-28 10:34:53 -0400

    changed to nullenc0de's directory list

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ pip3 install tldextract
 
 
 mkdir /root/wordlists
-wget https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt -O /root/wordlists/endpoints.txt
+wget https://gist.githubusercontent.com/nullenc0de/96fb9e934fc16415fbda2f83f08b28e7/raw/146f367110973250785ced348455dc5173842ee4/content_discovery_nullenc0de.txt -O /root/wordlists/endpoints.txt
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -O /root/wordlists/subdomains.txt
 wget https://raw.githubusercontent.com/OWASP/Amass/master/examples/config.ini -O /root/config.ini
 mkdir /root/githubkey/; echo 71356b8cfdadd4e051ac44776aac88321be31d19 > /root/githubkey/key.txt
